55bd3964001c71f18717a6ccf30bd67fb1a212a8
ricky <rickysahu@gmail.com>
2017-09-21 11:44:48 -0400

    more forgiving components

--- components/fhir/condition.js
+++ components/fhir/condition.js
@@ -6,20 +6,24 @@ export default class FhirCondition extends React.Component {
     return (
       <div className='tile'>
         <h2 className='primarycolor' style={{display: 'inline-block'}}>
-          ({this.props.resource.code.coding[0].code}) {this.props.resource.code.coding[0].display}
+          {this.props.resource.code && this.props.resource.code.coding ? <span>{this.props.resource.code.coding[0].display} {this.props.resource.code.coding[0].code}</span> : this.props.resource.code.text}
         </h2>
         &nbsp;&nbsp;&nbsp;
         <h3 style={{display: 'inline-block'}}>{this.props.resource.verificationStatus} & {this.props.resource.clinicalStatus}</h3>
         <table>
           <tbody>
-            <tr>
-              <td><strong>Onset</strong></td>
-              <td>{this.props.resource.onsetDateTime.slice(0,10)}</td>
-            </tr>
-            <tr>
-              <td><strong>Diagnosed by</strong></td>
-              <td>{this.props.resource.asserter.display}</td>
-            </tr>
+            {this.props.resource.onsetDateTime ? (
+              <tr>
+                <td><strong>Onset</strong></td>
+                <td>{this.props.resource.onsetDateTime.slice(0,10)}</td>
+              </tr>
+            ) : ''}
+            {typeof this.props.resource.asserter !== 'undefined' ? (
+              <tr>
+                <td><strong>Diagnosed by</strong></td>
+                <td>{this.props.resource.asserter.display}</td>
+              </tr>
+            ) : ''}
           </tbody>
         </table>
       </div>

cd1509a4b39b4ba3e3caf9bd91a7aa00d1a1829f
ricky1uphealth <ricky@1up.health>
2019-06-05 14:13:13 -0400

    update styles

--- pages/test.js
+++ pages/test.js
@@ -2,17 +2,22 @@ import React from 'react'
 import Link from 'next/link'
 import Layout from "../components/layouts/Layout"
 import Header from '../components/Header.js'
-import DummyComponent from 'fhir-react'
-import dstu2Encounter from '../tests/fixtures/dstu2Encounter.json'
+import {Fancy, TodoList, Patient, Resources} from 'fhir-react'
+import dstu2Encounter from '../tests/fixtures/dstu2/Encounter.json'
+import dstu2Patient from '../tests/fixtures/dstu2/Patient.json'
 
 export default class Home extends React.Component {
   render () {
     return (
       <Layout>
         <Header user={this.props.user} />
-        <h1>{`Testing`}</h1>
-        <DummyComponent />
-        {JSON.stringify(dstu2Encounter, null, 2)}
+        <div className='container'>
+          <h1>{`Testing`}</h1>
+          <Resources.Patient fhirResource={dstu2Patient.entry[0].resource} thorough={false}/>
+          {dstu2Encounter.entry.map(function(Encounter){
+            return JSON.stringify(dstu2Encounter, null, 2)
+          })}
+        </div>
       </Layout>
     )
   }
